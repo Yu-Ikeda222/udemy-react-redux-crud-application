9a2203355680925a9eadc0985b1d08ba97d51f13
Yu Ikeda <yuikeda@YunoMacBook-Air.local>
2020-12-02 02:53:33 +0900

    components learn

--- udemy-react-redux-crud-application/src/App.js
+++ udemy-react-redux-crud-application/src/App.js
@@ -1,7 +1,19 @@
-function App() {
-    return <input type="text" onClick={() => {console.log("iaiaiaiai")}} ></input>;
+// import React from 'react';
 
+// function App() {
+//     return <input type="text" onClick={() => {console.log("iaiaiaiai")}} ></input>;
+
+// }
+
+const App = () => {
+  return <div>nya--</div>
 }
-
-
+// const App = () => {
+//   return 
+//   <div>
+//     <Cat></Cat>
+//     <Cat></Cat>
+//     <Cat></Cat>
+//     </div>
+// }
 export default App;
