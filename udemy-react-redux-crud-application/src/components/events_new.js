81beab21ec09a971cccf05683e41b2b13eddde2d
Yu Ikeda <yuikeda@YunoMacBook-Air.local>
2020-12-04 22:57:26 +0900

    implement ,material-ui to events_new and events_show components

--- udemy-react-redux-crud-application/src/components/events_new.js
+++ udemy-react-redux-crud-application/src/components/events_new.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { postEvents } from '../actions'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 class EventsNew extends React.Component  {
   constructor(props) {
     super(props)
@@ -12,10 +14,15 @@ class EventsNew extends React.Component  {
   renderField(field) {
     const { input, label, type, meta: { touched, error}} = field
     return (
-    <div>
-      <input {...input} placeholder={label} type={type} />
-    {touched && error && <span>{error}</span>}
-    </div>)
+      <TextField 
+      hintText={label}
+      floatingLabelText={label}
+      type={type}
+      errorText={touched && error}
+      {...input}
+      fullWidth={true}
+    />
+    )
   }
 
   async onSubmit(values) {
@@ -25,6 +32,9 @@ class EventsNew extends React.Component  {
 
   render() {
     const { handleSubmit, pristine, submitting , invalid} = this.props
+    const style = {
+      margin:12
+    }
     return (
     <React.Fragment>
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -34,10 +44,8 @@ class EventsNew extends React.Component  {
         <div>
           <Field label="Body" name="body" type="text" component={this.renderField} />
         </div>
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/">Cancel</Link>
-        </div>
+        <RaisedButton label="Submit" type="Submit" style={style} disabled={pristine || submitting || invalid}  />
+        <RaisedButton label="Cancel"  style={style} containerElement={<Link to="/" />} />
       </form>
     </React.Fragment>
       
--- udemy-react-redux-crud-application/src/components/events_show.js
+++ udemy-react-redux-crud-application/src/components/events_show.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { getEvents, deleteEvents, putEvents } from '../actions'
 import { Link } from 'react-router-dom'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
 class EventsShow extends React.Component  {
   constructor(props) {
     super(props)
@@ -19,10 +22,15 @@ class EventsShow extends React.Component  {
   renderField(field) {
     const { input, label, type, meta: { touched, error}} = field
     return (
-    <div>
-      <input {...input} placeholder={label} type={type} />
-    {touched && error && <span>{error}</span>}
-    </div>)
+      <TextField 
+      hintText={label}
+      floatingLabelText={label}
+      type={type}
+      errorText={touched && error}
+      {...input}
+      fullWidth={true}
+    />
+    )
   }
 
   async onDeleteClick(){
@@ -37,6 +45,9 @@ class EventsShow extends React.Component  {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = {
+      margin:12
+    }
     return (
     <React.Fragment>
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -47,9 +58,10 @@ class EventsShow extends React.Component  {
           <Field label="Body" name="body" type="text" component={this.renderField} />
         </div>
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid } />
-          <Link to="/">Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
+        <RaisedButton label="Submit" type="Submit" style={style} disabled={pristine || submitting || invalid}  />
+        <RaisedButton label="Cancel"  style={style} containerElement={<Link to="/" />} />
+    <RaisedButton label="Delete"  style={style} onClick={this.onDeleteClick}containerElement={<Link to="/"  />} />
+          
         </div>
       </form>
     </React.Fragment>
